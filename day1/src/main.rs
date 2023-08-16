3a84356a83103926fa987947c7f8f6b20b061c67
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-08-16 20:21:53 +0300

    complete day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -14,8 +14,23 @@ fn main() {
         let current_sum = set.lines().filter_map(|line| line.parse::<i32>().ok()).sum();
 
         if current_sum > top_sums[0]{
+
             top_sums[0] = current_sum;
+
             println!("set with the largest sum is: {:?}", top_sums[0]);
+
+        } else if current_sum > top_sums[1] {
+            
+            top_sums[1] = current_sum;
+
+            println!("second largest sum is: {:?}", top_sums[1]);
+
+        } else if current_sum > top_sums[2] {
+            
+            top_sums[2] = current_sum;
+
+            println!("third largest sum is: {:?}", top_sums[2]);
+            
         }
 
     }
